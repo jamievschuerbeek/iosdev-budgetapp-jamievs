b65039cc006795fa8ae224efc198ac6d5e3a8215
Jamie Van Schuerbeek <jamie.vanschuerbeek@student.hogent.be>
2023-12-27 01:40:06 +0100

    Initial Commit

---
+++ budgetApp/budgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetAppApp.swift
+//  budgetApp
+//
+//  Created by Jamie Van Schuerbeek on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct budgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
