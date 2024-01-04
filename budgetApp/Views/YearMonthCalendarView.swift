12a77019cd4f58eec7709a0deda33df41530aedd
Jamie Van Schuerbeek <jamie.vanschuerbeek@student.hogent.be>
2024-01-04 16:55:21 +0100

    updated source desc

--- budgetApp/Views/YearMonthCalendarView.swift
+++ budgetApp/Views/YearMonthCalendarView.swift
@@ -1,5 +1,5 @@
 //
-//  YearMonthCalendarView.swift
+//  YearMonthView.swift
 //  budgetApp
 //
 //  Created by Jamie Van Schuerbeek on 04/01/2024.
@@ -7,10 +7,7 @@
 
 import SwiftUI
 
-
-//SOURCE: https://stackoverflow.com/questions/64093723/swiftui-date-picker-display-year-only
 struct YearMonthCalendarView: View {
-    
     @State var selection: Int = Int(Date().formatted(.dateTime.year())) ?? 0
     @State var monthSelection: String = Date().formatted(.dateTime.month(.abbreviated))
     @State var currentYear = Int(Date().formatted(.dateTime.year())) ?? 0
@@ -27,6 +24,7 @@ struct YearMonthCalendarView: View {
                     Text(months[value])
                 }
             }
+            //SOURCE: https://stackoverflow.com/questions/64093723/swiftui-date-picker-display-year-only
             Picker("", selection: $selection) {
                 ForEach(2023...currentYear, id: \.self) {
                     Text(String($0))
